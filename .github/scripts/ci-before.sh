6e30dd14c5b4a9431f99ea2b4f874d6da45972e6
序列号 <2402324010@qq.com>
2023-07-04 21:32:08 +0800

    Update ci-before.sh

--- .github/scripts/ci-before.sh
+++ .github/scripts/ci-before.sh
@@ -8,12 +8,12 @@ mkdir -p ${HOME}/sync
   cp imgsync ${HOME}/sync/
 } || {
   # 出现bug的时候直接使用docker的二进制文件，而不用提交代码触发ci
-  docker run --rm -tid --name tool zhangguanzhang/google_containers_imgsync top
+  docker run --rm -tid --name tool xlh001/google_containers_imgsync top
   docker cp tool:/imgsync ${HOME}/sync/
   docker kill tool
 }
 
-docker run --rm -tid --name db zhangguanzhang/google_containers_db top
+docker run --rm -tid --name db xlh001/google_containers_db top
 docker cp db:/bolt.db ${HOME}/sync/
 
 docker kill db
